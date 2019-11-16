9340d4f7828a6df295a731b296b9b35bd8d59554
cesarsld <jaimes.cesar88@gmail.com>
2019-11-16 23:26:16 +0100

    phpmyadminadded

--- srcs/boot.sh
+++ srcs/boot.sh
@@ -6,12 +6,10 @@ apt-get -y install wget
 apt-get -y install php php-fpm php-mysql php-common php-cli php-common php-json php-opcache php-readline
 
 
-wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.tar.gz
-tar -zxvf phpMyAdmin-4.9.0.1-all-languages.tar.gz
 
 mkdir -p /var/www/localhost
 cp /localhost-conf /etc/nginx/sites-available/localhost
-ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/default
+ln -fs /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/default
 
 #DATABASE SETUP
 service mysql start
@@ -20,14 +18,23 @@ echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost';" | mysql -u roo
 echo "FLUSH PRIVILEGES;" | mysql -u root
 echo "update mysql.user set plugin = 'mysql_native_password' where user='root';" | mysql -u root
 cd
-mysql wordpress -u root --password=  < wordpress.sql
+mysql wordpress -u root --password=  < /wordpress.sql
 
 #WORDPRESS INSTALL
 cd
-cp wordpress.tar.gz /var/www/localhost/
-cd /var/www/localhost/
-tar -xf wordpress.tar.gz
-rm wordpress.tar.gz
+cp /wordpress-5.3.tar.gz /var/www/
+cd /var/www
+tar -xf wordpress-5.3.tar.gz
+rm wordpress-5.3.tar.gz
+rmdir localhost
+mv wordpress localhost
+
+#PHPMYADMIN INSTALL
+cd
+wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-english.tar.gz
+mkdir /var/www/localhost/phpmyadmin
+tar xzf phpMyAdmin-4.9.0.1-english.tar.gz --strip-components=1 -C /var/www/localhost/phpmyadmin
+cp /config.inc.php /var/www/localhost/phpmyadmin/
 
 chown -R www-data:www-data /var/www/*
 chmod -R 755 /var/www/*
